5042a70fdfec2541c155e3d70e8697185e3e663c
Katie Coleman <katiecoleman@Katies-Laptop.local>
2024-05-12 02:04:04 -0500

    update state to try to resolve rendering issue

--- src/partials/Questions.jsx
+++ src/partials/Questions.jsx
@@ -28,6 +28,7 @@ const Questions = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [activeTab, setActiveTab] = useState("question");
   const [generatePDF, setGeneratePDF] = useState(false);
+  const [sortedCollection, setSortedCollection] = useState([]);
   const ref = useRef();
   const { toPDF, targetRef } = usePDF({ filename: "page.pdf" });
 
@@ -70,6 +71,19 @@ const Questions = () => {
 
   useEffect(() => {
     localStorage.setItem("myCollection", JSON.stringify(collection));
+    const sortedCollectionList = collection.sort(
+      //sort by goals, values, then questions then by important
+      (a, b) => {
+        if (a.type === "goals" && b.type !== "goals") return -3;
+        if (a.type === "values" && b.type === "question") return -2;
+        if (a.type === "values" && b.type === "goals") return -1;
+        if (a.type === "question" && b.type !== "question") return 1;
+        if (a.important && !b.important) return -1;
+        if (!a.important && b.important) return 1;
+        return 0;
+      }
+    );
+    setSortedCollection(sortedCollectionList);
   }, [collection]);
 
   useEffect(() => {
@@ -229,81 +243,67 @@ const Questions = () => {
             Questions List
           </div>
         )}
-        {collection.length > 0 ? (
+        {sortedCollection.length > 0 ? (
           <div
             className={`grid grid-cols-1 md:grid-cols-2 ${
               generatePDF ? "lg:grid-cols-1" : ""
             } gap-8 mt-5`}
           >
-            {collection.length > 0 &&
-              collection
-                .sort(
-                  //sort by goals, values, then questions then by important
-                  (a, b) => {
-                    if (a.type === "goals" && b.type !== "goals") return -3;
-                    if (a.type === "values" && b.type === "question") return -2;
-                    if (a.type === "values" && b.type === "goals") return -1;
-                    if (a.type === "question" && b.type !== "question")
-                      return 1;
-                    if (a.important && !b.important) return -1;
-                    if (!a.important && b.important) return 1;
-                    return 0;
-                  }
-                )
-                .map((item, index) => (
-                  <div
-                    key={index}
-                    className={`p-4 bg-white pb-10 shadow-lg rounded border ${
-                      generatePDF ? "max-h-32" : ""
-                    }`}
-                  >
-                    <div className="w-full flex flex-row gap-2 justify-between self-center">
-                      <div className="text-xl font-semibold self-center">
-                        <FontAwesomeIcon
-                          icon={faIconLookup[item.type]}
-                          className="w-6 h-6 text-red-200"
-                        />
-                      </div>
-                      <div className="text-xl font-semibold self-center text-left w-full flex">
-                        {item.title}
-                      </div>
-                      <div className="">
-                        {item.important && (
-                          <FontAwesomeIcon
-                            icon={faStar}
-                            className="w-6 h-6 text-orange-200"
-                          />
-                        )}
-                      </div>
+            {sortedCollection.length > 0 &&
+              sortedCollection.map((item, index) => (
+                <div
+                  key={index}
+                  className={`p-4 bg-white pb-10 shadow-lg rounded border ${
+                    generatePDF ? "max-h-32" : ""
+                  }`}
+                >
+                  <div className="w-full flex flex-row gap-2 justify-between self-center">
+                    <div className="text-xl font-semibold self-center">
+                      <FontAwesomeIcon
+                        icon={faIconLookup[item.type]}
+                        className="w-6 h-6 text-red-200"
+                      />
                     </div>
-
-                    {item.type === "question" && (
-                      <>
-                        <hr className="my-2" />
-                        <div className="overflow-x-auto flex flex-col lg:flex-row justify-between ">
-                          <div className="w-full">
-                            <table className="mt-1 mb-2 ml-2">
-                              <tbody className="w-full ">
-                                <tr className="">
-                                  <td className="text-left font-medium w-full text-xs">
-                                    Area of expertise?{" "}
-                                    <span className="pr-5 pl-2">Yes</span>{" "}
-                                    <span> No</span>
-                                  </td>
-                                </tr>
-                                <tr className="">
-                                  <td className="text-left font-medium w-full">
-                                    Answer:
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                          </div>
-                        </div>
-                      </>
-                    )}
+                    <div className="text-xl font-semibold self-center text-left w-full flex">
+                      {item.title}
+                    </div>
+                    <div className="">
+                      {item.important && (
+                        <FontAwesomeIcon
+                          icon={faStar}
+                          className="w-6 h-6 text-orange-200"
+                        />
+                      )}
+                    </div>
                   </div>
-                ))}
+
+                  {item.type === "question" && (
+                    <>
+                      <hr className="my-2" />
+                      <div className="overflow-x-auto flex flex-col lg:flex-row justify-between ">
+                        <div className="w-full">
+                          <table className="mt-1 mb-2 ml-2">
+                            <tbody className="w-full ">
+                              <tr className="">
+                                <td className="text-left font-medium w-full text-xs">
+                                  Area of expertise?{" "}
+                                  <span className="pr-5 pl-2">Yes</span>{" "}
+                                  <span> No</span>
+                                </td>
+                              </tr>
+                              <tr className="">
+                                <td className="text-left font-medium w-full">
+                                  Answer:
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </div>
+              ))}
           </div>
         ) : (
           <div className="p-4 bg-white pb-10 shadow-lg rounded mt-7">
